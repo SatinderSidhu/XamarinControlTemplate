4d5f7f122ebee1cd0fb3894fb7c0ca3181231af7
Satinder Sidhu <Satinder.satty@gmail.com>
2018-01-21 15:20:05 -0500

    Control Templates learning

---
+++ ControlTemplatesLearning/Page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ControlTemplatesLearning
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Master_Clicked(object sender, System.EventArgs e)
+        {
+            this.ControlTemplate = (ControlTemplate)App.Current.Resources["ThemeMaster"];
+
+        }
+
+        void Handle_Blue_Clicked(object sender, System.EventArgs e)
+        {
+            this.ControlTemplate = (ControlTemplate)App.Current.Resources["ThemeBlue"];
+        }
+    }
+}
